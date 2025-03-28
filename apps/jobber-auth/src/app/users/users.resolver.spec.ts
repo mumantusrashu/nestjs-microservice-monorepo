55041f428953f7f3c188155ba146fc1ed637b281
mumantusrashu <rashu.shreyansh@mumantus.com>
2025-03-28 17:20:10 +0530

    fix: fixed failing test cases

--- apps/jobber-auth/src/app/users/users.resolver.spec.ts
+++ apps/jobber-auth/src/app/users/users.resolver.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserResolver } from './users.resolver';
+import { UserService } from './users.service';
 
 describe('UserResolver', () => {
   let resolver: UserResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserResolver],
+      providers: [
+        UserResolver,
+        {
+          provide: UserService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     resolver = module.get<UserResolver>(UserResolver);
