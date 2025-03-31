609f635956ff07ac415d32a35a552a3ba7e2813e
mumantusrashu <rashu.shreyansh@mumantus.com>
2025-03-31 22:21:01 +0530

    added jobber jobs microservice

--- apps/jobber-jobs/src/main.ts
+++ apps/jobber-jobs/src/main.ts
@@ -3,11 +3,12 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix(globalPrefix);
-  const port = app.get(ConfigService).getOrThrow('PORT') || 3000;
+  const port = app.get(ConfigService).getOrThrow('PORT') || 3001;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
